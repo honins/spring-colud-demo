b9116233a67b435b47810d658a9a90ea25db99e5
honins <honins@qq.com>
2020-07-08 16:54:38 +0800

    添加组件zuul，用于请求过滤和请求转发

---
+++ service-zuul/src/main/java/com/hy/hoxton/zuul/ServiceZuulApplication.java
@@ -0,0 +1,23 @@
+package com.hy.hoxton.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Created by hy
+ * @date on 2020/7/8 16:29
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceZuulApplication.class, args );
+    }
+
+}
---
+++ service-zuul/src/main/java/com/hy/hoxton/zuul/component/MyFilter.java
@@ -0,0 +1,52 @@
+package com.hy.hoxton.zuul.component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Created by hy
+ * @date on 2020/7/8 16:40
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        if (token == null){
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){
+                return null;
+            }
+        }
+        return null;
+    }
+}
