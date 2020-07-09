b810d2e36f1a6a8db0e0417148a216b29d8c271c
honins <honins@qq.com>
2020-07-09 09:01:12 +0800

    添加网关框架gateway，取代zuul网关。功能有路由转发、权限校验、限流控制等

---
+++ config-server/src/main/java/com/hy/hoxton/config/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.hy.hoxton.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Created by hy
+ * @date on 2020/7/8 17:03
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
