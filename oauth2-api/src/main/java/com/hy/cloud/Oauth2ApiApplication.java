e380675416a35a25e5ba17c543fff6f8204fdc00
honins <honins@qq.com>
2020-08-21 09:44:27 +0800

    修改auth-service模块名为oauth2-service，作为认证服务中心 添加oauth2-api模块，作为使用提供接口功能的服务

---
+++ oauth2-api/src/main/java/com/hy/cloud/Oauth2ApiApplication.java
@@ -0,0 +1,17 @@
+package com.hy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Created by hy
+ * @date on 2020/8/21 9:06
+ */
+@SpringBootApplication
+public class Oauth2ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ApiApplication.class, args);
+    }
+
+}
---
+++ oauth2-api/src/main/java/com/hy/cloud/pojo/UserDTO.java
@@ -0,0 +1,19 @@
+package com.hy.cloud.pojo;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * Created by macro on 2020/6/19.
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private List<String> roles;
+}
